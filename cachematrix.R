e6ffca95d4bc69f03aed81b1313b2f336cbdfa9b
Asad Bin Imtiaz <Asadbin.Imtiaz2@swisscom.com>
2020-08-01 10:13:15 +0200

    Commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_matrix <- NULL
+  set <- function (y) {
+    x <<- y
+    inv_matrix <<- cacheSolve(y)
+  }
+  get <- function () { x }
+  setInverse <- function (z) 
+  {
+    inv_matrix <<- z
+    x <<- cacheSolve(z)
+  }
+  getInverse <- function () { 
+    if(is.null(inv_matrix)){
+      inv_matrix <<- cacheSolve(x)
+    }
+    inv_matrix 
+  }
+  
+  
+  list (set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv_matrix <- solve(x)
+  
+  inv_matrix
 }
