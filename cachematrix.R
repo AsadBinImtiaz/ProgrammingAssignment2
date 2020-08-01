91bd2dfaa907df2625397108f39add35ac5eb063
Asad Bin Imtiaz <Asadbin.Imtiaz2@swisscom.com>
2020-08-01 10:34:54 +0200

    Assignemnt 2

--- cachematrix.R
+++ cachematrix.R
@@ -7,18 +7,15 @@ makeCacheMatrix <- function(x = matrix()) {
   inv_matrix <- NULL
   set <- function (y) {
     x <<- y
-    inv_matrix <<- cacheSolve(y)
+    inv_matrix <<- NULL # Calculate on demand on Get
   }
   get <- function () { x }
   setInverse <- function (z) 
   {
     inv_matrix <<- z
-    x <<- cacheSolve(z)
+    #x <<- cacheSolve(z)
   }
   getInverse <- function () { 
-    if(is.null(inv_matrix)){
-      inv_matrix <<- cacheSolve(x)
-    }
     inv_matrix 
   }
   
@@ -27,11 +24,18 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
+
+
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  inv_matrix <- solve(x)
+  inv_matrix <- x$getInverse()
+  
+  if(!is.null(inv_matrix))
+    return(inv)
+  
+  x$setInverse(solve(x$get()))
   
   inv_matrix
 }
